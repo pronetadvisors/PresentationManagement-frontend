d6eb3adfad5580fd8e16466df5c451760f83a28f
Raajheer1 <raaj.patel229@gmail.com>
2022-11-06 21:35:14 -0600

    More --- SPAAAACE ---

--- src/views/page/Displays.vue
+++ src/views/page/Displays.vue
@@ -5,7 +5,7 @@
       alt="Logo"
       class="max-w-full align-center mx-auto"
     />
-    <div class="grid grid-cols-5 gap-4 mb-5 mt-5 pb-3 border-b-2 border-emerald-700">
+    <div class="grid grid-cols-5 gap-4 mb-5 mt-5 pb-5 border-b-2 border-emerald-700">
       <div class="text-white text-left col-span-2">
         <h1 class="text-6xl font-light">
           {{ date.toLocaleTimeString([], {
